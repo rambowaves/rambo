195581d6a23390e4222937fcd4ffe0beb9d03d66
Benjamin Magos <bjkmagos@hotmail.com>
2014-02-11 17:07:46 -0800

    Two click update. Clears between shots.
    Fixed the setInterval so that it has a clearInterval that fires after
    the line has drawn. Now that there is a clearInterval, it will only
    draw one line at a time.
    
    BUG: If you try to draw lines really rapidly, it will start a new loop
    before the clearInterval of the previous loop has been called,
    therefore creating an endless loop of the first one while you go on
    drawing new lines. Fix coming soon.

--- drawLine/drawLine.js
+++ drawLine/drawLine.js
@@ -5,6 +5,8 @@
 var canvas = document.getElementById("canvas");
 canvas.addEventListener('click', draw, false);
 window.addEventListener('resize', resizeCanvas, false);
+context = canvas.getContext("2d");
+var stopWhen;
 
 function clickPosition(e) {
     var x;
@@ -24,25 +26,42 @@ function resizeCanvas() {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
 }
-
+var f = 0;
 function draw(e) {
-    context = canvas.getContext("2d");
+    context.clearRect(0,0,canvas.width,canvas.height);
+    if (f === 0) {
+        f = e;
+    }
+    else {
+        actuallyDraw(e, f);
+        f = 0;
+    }
+}
+var amount = 0;
+function actuallyDraw(e, f){
     var pointx = clickPosition(e)[0];
     var pointy = clickPosition(e)[1];
-    
-    var x = canvas.width / 2;
-    var y = canvas.height / 2;
+    var x = clickPosition(f) [0];
+    var y = clickPosition(f) [1];
 
     console.log(x + " maps to x " + pointx);
     console.log(y + " maps to y " + pointy);
     
-    var amount = 0;
-    setInterval(function() {
+    context.beginPath();
+    stopWhen = setInterval("loop("+x+","+y+","+pointx+","+pointy+")", 30);
+    setTimeout(function() {
+        clearInterval(stopWhen);
+        amount = 0;
+    }, 360);
+    
+}
+
+function loop(x,y,pointx,pointy){
     amount += 0.1; // change to alter duration
     if (amount > 1) amount = 1;
     context.moveTo(x, y);
     context.lineTo(x + (pointx - x) * amount, y + (pointy - y) * amount);
     context.strokeStyle = "white";
     context.stroke();
-}, 30);
+    console.log(amount);
 }
